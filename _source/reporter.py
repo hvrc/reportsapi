a7064d4a70f5cfdcbb5ce7270bf79f62d3fb42d6
hvrc <harshavardhan@rajmachikar.com>
2019-06-10 15:18:59 +0530

    changes

--- _source/reporter.py
+++ _source/reporter.py
@@ -9,17 +9,23 @@ import pymysql
 def getDataframe(input):
     return pd.read_csv(input, dtype=object, encoding="ISO 8859-1") if isinstance(input, str) else pd.read_sql_query("SELECT * FROM %s" % (str(input[5])), pymysql.connect(host=str(input[0]), user=str(input[1]), password=str(input[2]), database=str(input[3]), port=int(input[4])))
 
+# Converts a pandas dataframe to a csv and creates it at the output path
+def createCSV(df, path, sep=","):
+    df.to_csv(path, sep=sep, encoding="utf-8")
+
 # Converts a normalized dataframe to a dataframe displaying the frequency of each
 # instance, categorized by the columns specified in cols, and returns it; if type
 # is "frequency", otherwise, function returns a dataframe displaying only the columns
 # specified in cols, indexed by the first column specified in cols
 def getReport(df, cols, type="frequency"):
+    # format dates here
     df = formatDates(df, cols[0], "%d-%m-%Y")
     return pd.DataFrame(df.groupby(cols)[df.columns[0]].count()) if len(cols) == 1 else df.groupby(cols)[df.columns[0]].count().unstack().fillna(0) if type == "frequency" else df[cols[1:]].set_index(df[cols[0]])
 
 # Returns a 'buffer' from a pandas dataframe. The buffer data is used by 'csvreport'
 # to generate a csv in the view
 def getBuffer(df, sep=","):
+    # use utils functions here
     buffer = io.StringIO()
     df.to_csv(buffer, sep=sep, encoding="utf-8")
     buffer.seek(0)
